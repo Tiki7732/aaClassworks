3816639f42a77ae5133f0e666adcd050830dd699
Laurent Saidah <lsaidah@gmail.com>
2020-05-22 13:26:36 -0500

    making a queue

---
+++ ADTs/queue.rb
@@ -0,0 +1,22 @@
+class Queue
+
+    private 
+    attr_reader :que_arr
+    public
+
+    def initialize
+        @que_arr = []
+    end
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
+
+    end
+
+    def peek
+
+    end
+end
