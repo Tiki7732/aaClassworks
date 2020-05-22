c8061ecd7735faed00224d114ace2f11a1229559
Laurent Saidah <lsaidah@gmail.com>
2020-05-22 13:31:52 -0500

    Filled in the queue methods

--- ADTs/queue.rb
+++ ADTs/queue.rb
@@ -9,14 +9,24 @@ class Queue
     end
 
     def enqueue(el)
-
+        que_arr.unshift(el)
+        el
     end
 
     def dequeue
-
+        que_arr.pop
     end
 
     def peek
-
+        que_arr.last
     end
 end
+
+quimb = Queue.new
+quimb.enqueue("Banana")
+quimb.enqueue("claire")
+quimb.enqueue("dog")
+p quimb
+quimb.dequeue
+p quimb
+p quimb.peek
