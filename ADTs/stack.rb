4959911876dce623f14b3df93c089f3783d87bcc
Laurent Saidah <lsaidah@gmail.com>
2020-05-22 13:22:08 -0500

    filled in the methods

--- ADTs/stack.rb
+++ ADTs/stack.rb
@@ -1,17 +1,29 @@
 class Stack
+    private
+    attr_reader :stack_arr
+    public
     def initialize
-
+        @stack_arr = []
     end
 
     def push(el)
-
+        stack_arr.push(el)
     end
 
     def pop
-
+        stack_arr.pop
     end
 
     def peek
-
+        stack_arr.last
     end
 end
+
+# sam = Stack.new
+# sam.push("cat")
+# sam.push("dog")
+# sam.push("house")
+# p sam
+# sam.pop
+# p sam
+# p sam.peek
