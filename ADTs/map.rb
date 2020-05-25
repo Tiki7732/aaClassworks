5b6a509d7b394957a1e10069cb5c9154fdd86891
Laurent Saidah <lsaidah@gmail.com>
2020-05-25 13:21:20 -0500

    filled in the set method

--- ADTs/map.rb
+++ ADTs/map.rb
@@ -8,7 +8,15 @@ class Map
     end
 
     def set(key, value)
-
+        pair = [key, value]
+        selected = nil
+        @map_arr.each do |pairs|
+            if pairs.first == key
+                selected = pairs
+                pairs[-1] = value
+            end
+        end
+        @map_arr.push(pair) if selected.nil?
     end
 
     def get(key)
@@ -23,3 +31,10 @@ class Map
 
     end
 end
+
+m = Map.new
+m.set("blah", 5)
+m.set("wow", 8)
+p m
+m.set("blah", 2)
+p m
