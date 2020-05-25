4cf82df75ad260fde2566ce9977f15dad1e45d7a
Laurent Saidah <lsaidah@gmail.com>
2020-05-25 13:39:24 -0500

    filled in the rest of the methods

--- ADTs/map.rb
+++ ADTs/map.rb
@@ -10,26 +10,29 @@ class Map
     def set(key, value)
         pair = [key, value]
         selected = nil
-        @map_arr.each do |pairs|
+        map_arr.each do |pairs|
             if pairs.first == key
                 selected = pairs
                 pairs[-1] = value
             end
         end
-        @map_arr.push(pair) if selected.nil?
+        map_arr.push(pair) if selected.nil?
     end
 
     def get(key)
-
+        map_arr.each {|pairs| return pairs.last if pairs.first == key}
+        nil
     end
 
     def delete(key)
-
+        map_arr.reject!{|pair| pair if pair.first == key}
     end
 
     def show
-
+        print map_arr
+        print "\n"
     end
+
 end
 
 m = Map.new
@@ -38,3 +41,8 @@ m.set("wow", 8)
 p m
 m.set("blah", 2)
 p m
+p m.get("blah")
+m.set("garg", 10)
+m.delete("blah")
+p m
+m.show
