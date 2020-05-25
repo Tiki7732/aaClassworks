c2a7345081523a07f8bee71ffcf8162bcfd7f35f
Laurent Saidah <lsaidah@gmail.com>
2020-05-25 13:09:10 -0500

    creating the methods for map class

--- ADTs/map.rb
+++ ADTs/map.rb
@@ -1,3 +1,25 @@
 class Map
 
+    private
+    attr_reader :map_arr
+    public
+    def initialize
+        @map_arr = []
+    end
+
+    def set(key, value)
+
+    end
+
+    def get(key)
+
+    end
+
+    def delete(key)
+
+    end
+
+    def show
+
+    end
 end
