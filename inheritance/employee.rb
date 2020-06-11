ed819285f80a833bdd5303fb1e2c3729c66d2948
tiki7732 <lsaidah@gmail.com>
2020-06-11 14:00:05 -0500

    filling out initialize and adding manager sub class

--- inheritance/employee.rb
+++ inheritance/employee.rb
@@ -1,3 +1,32 @@
 class Employee
 
+    attr_reader :name, :title, :salary, :boss
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        amount = salary * multiplier
+        return amount
+    end
 end
+
+class Manager < Employee
+    
+    attr_reader :employees
+    def initialize(name, title, salary, boss, employees)
+        super(name, title, salary, boss)
+        @employees = employees
+    end
+
+end
+
+ed = Employee.new("Ed", "SalesRep", 50000, "Tom")
+p ed.boss
+p ed.bonus(2)
+tom = Manager.new("Tom", "SalesManager", 75000, "Kathy", [ed])
+p tom.boss
+p tom.employees
